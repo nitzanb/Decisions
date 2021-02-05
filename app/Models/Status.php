cbc2d2aaecbb87823b531cec361cf1efe240d813
Nitzan Brumer <nitzanb@gmail.com>
2021-02-06 01:17:29 +0200

    Added softDelete to status model
    Not deleting statuses from Db, Just adding deleted_at

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -9,6 +9,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Status extends Model
 {
     use HasFactory;
-    use SoftDeletes;
+    use SoftDeletes; //Won't delete a row from the DB, just add a timestamp at `deleted_at`
     protected $fillable = ['id', 'name', 'description'];
 }
