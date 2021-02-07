0b79bbea694e01074af434128faf233b2ee87b29
Nitzan Brumer <nitzanb@gmail.com>
2021-02-07 23:42:26 +0200

    Added Team support to new layout

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -23,6 +23,64 @@
                 </div>
               </div>
 
+             <!-- Teams -->
+         <!-- Teams Dropdown -->
+                                <jet-dropdown align="right" width="60" v-if="$page.props.jetstream.hasTeamFeatures">
+                                    <template #trigger>
+                                        <span class="inline-flex rounded-md">
+                                            <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                                {{ $page.props.user.current_team.name }}
+
+                                                <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                                    <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                                </svg>
+                                            </button>
+                                        </span>
+                                    </template>
+
+                                    <template #content>
+                                        <div class="w-60">
+                                            <!-- Team Management -->
+                                            <template v-if="$page.props.jetstream.hasTeamFeatures">
+                                                <div class="block px-4 py-2 text-xs text-gray-400">
+                                                    Manage Team
+                                                </div>
+
+                                                <!-- Team Settings -->
+                                                <jet-dropdown-link :href="route('teams.show', $page.props.user.current_team)">
+                                                    Team Settings
+                                                </jet-dropdown-link>
+
+                                                <jet-dropdown-link :href="route('teams.create')" v-if="$page.props.jetstream.canCreateTeams">
+                                                    Create New Team
+                                                </jet-dropdown-link>
+
+                                                <div class="border-t border-gray-100"></div>
+
+                                                <!-- Team Switcher -->
+                                                <div class="block px-4 py-2 text-xs text-gray-400">
+                                                    Switch Teams
+                                                </div>
+
+                                                <template v-for="team in $page.props.user.all_teams">
+                                                    <form @submit.prevent="switchToTeam(team)" :key="team.id">
+                                                        <jet-dropdown-link as="button">
+                                                            <div class="flex items-center">
+                                                                <svg v-if="team.id == $page.props.user.current_team_id" class="mr-2 h-5 w-5 text-green-400" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                                                <div>{{ team.name }}</div>
+                                                            </div>
+                                                        </jet-dropdown-link>
+                                                    </form>
+                                                </template>
+                                            </template>
+                                        </div>
+                                    </template>
+                                </jet-dropdown>
+             <!-- End Teams -->
+
+
+
+
               <!-- right navbar -->
               <div class="flex items-center relative">
                 <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" class="fill-current mr-3 hover:text-blue-500"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2zm6-6v-5c0-3.07-1.63-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.64 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2zm-2 1H8v-6c0-2.48 1.51-4.5 4-4.5s4 2.02 4 4.5v6z"/></svg>
@@ -43,9 +101,15 @@
 </template>
 
 <script>
+  import JetDropdownLink from '@/Jetstream/DropdownLink'
+  import JetDropdown from '@/Jetstream/Dropdown'
 
-export default {
+  export default {
     name: 'Navbar',
+    components: {
+      JetDropdownLink,
+      JetDropdown,
+    },
     data() {
         return {
             dropDownOpen: false
@@ -54,7 +118,14 @@ export default {
     methods: {
         toggleSidebar() {
             sideBarOpen = !sideBarOpen;
-        }
+        },
+         switchToTeam(team) {
+                this.$inertia.put(route('current-team.update'), {
+                    'team_id': team.id
+                }, {
+                    preserveState: false
+                })
+            },
     }
 }
 </script>
